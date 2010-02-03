777f9e229ee70417aa344756917f37bea0dcb7da
rbauduin@myowndb.com <rbauduin@myowndb.com>
2010-02-03 07:47:57 +0100

    changed file field template for IE

--- src/gallery-form/js/file-field.js
+++ src/gallery-form/js/file-field.js
@@ -10,11 +10,23 @@ function FileField () {
 }
 
 Y.mix(FileField, {
-    NAME : 'file-field'
+    NAME : 'file-field',
+    INPUT_TEMPLATE: '<input type="file"/>'
 });
 
 Y.extend(FileField, Y.FormField, {
-    _nodeType : 'file'
+    _nodeType : 'file',
+	_renderFieldNode : function () {
+		var contentBox = this.get('contentBox'),
+			field = contentBox.query('#' + this.get('id'));
+				
+		if (!field) {
+			field = Y.Node.create(FileField.INPUT_TEMPLATE);
+			contentBox.appendChild(field);
+		}
+
+		this._fieldNode = field;
+	}
 });
 
 Y.FileField = FileField;
